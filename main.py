1e6a20a98cc621630b05f1e1cde56897d955920e
l1xnan <l1xnan@qq.com>
2023-04-19 10:54:36 +0800

    添加 profile 装饰器

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
+import time
+from functools import wraps
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
 import psutil
 from faker import Faker
-from memory_profiler import profile
 
 
 def process_memory():
@@ -73,44 +74,58 @@ def generate_test_data(size=100000, force=False):
     df.to_parquet("test.parquet", index=False)
 
 
-@profile_cpu
-@profile_mem
+stats_df = pd.DataFrame(columns=["time(s)", "memory(KB)"])
+
+
+def profile(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        start_time = time.perf_counter()
+        df = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        param = ",".join(args)
+        if kwargs:
+            param += "," + ",".join([f"{k}={v}" for k, v in kwargs.items()])
+        memory = df.memory_usage(index=False, deep=True).sum() / 1024
+        idx = f"{func.__name__}({param})"
+        stats_df.loc[idx] = [total_time, memory]
+        return df
+
+    return inner
+
+
 @profile
 def read_csv_by_python():
     df = pd.read_csv("test.csv", sep="\t", engine="python")
-    df.info(memory_usage="deep", verbose=False)
+    return df
 
 
-@profile_cpu
-@profile_mem
 @profile
 def read_csv_by_pyarrow():
     df = pd.read_csv("test.csv", sep="\t", engine="pyarrow", dtype_backend="pyarrow")
-    df.info(memory_usage="deep", verbose=False)
+    return df
 
 
-@profile_cpu
-@profile_mem
 @profile
 def read_parquet_by_default():
     df = pd.read_parquet("test.parquet")
-    df.info(memory_usage="deep", verbose=False)
+    return df
 
 
-@profile_cpu
-@profile_mem
 @profile
 def read_parquet_by_pyarrow():
     df = pd.read_parquet("test.parquet", engine="pyarrow", dtype_backend="pyarrow")
-    df.info(memory_usage="deep", verbose=False)
+    return df
 
 
 if __name__ == '__main__':
+    pd.set_option("display.max_columns", None)
+    pd.set_option("display.max_rows", None)
+    pd.set_option("display.width", 1000)
     generate_test_data(force=False)
     read_csv_by_python()
-    print("=" * 30)
     read_csv_by_pyarrow()
-    print("=" * 30)
     read_parquet_by_default()
-    print("=" * 30)
     read_parquet_by_pyarrow()
+    print(stats_df)
